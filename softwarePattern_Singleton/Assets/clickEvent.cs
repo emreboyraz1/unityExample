40de2d7b180e14bf2a3aaefd0c7c3dc373c155e0
emreboyrazco <info@emreboyraz.co>
2017-05-04 14:29:07 +0300

    vers1
    init

---
+++ softwarePattern_Singleton/Assets/clickEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class clickEvent : MonoBehaviour, IPointerClickHandler
+{
+    private singleton s;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        singleton.Instance.SecondCounter();
+    }
+  
+
+}
---
+++ softwarePattern_Singleton/Assets/singleton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Multithreaded Singleton
+
+//sealed to prevent derivation
+public sealed class singleton
+{
+
+    //to ensure that assignment to the instance variable completes before the instance variable can be accessed
+    private static volatile singleton instance;
+
+    //instance to lock on, rather than locking on the type itself, to avoid deadlocks
+    private static object syncRoot = new Object();
+
+    private singleton() { }
+
+    // Double-Check Locking 
+    public static singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                // lock block identifies
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = new singleton() ;
+                }
+            }
+            return instance;
+        }
+    }
+  
+    public void SecondCounter()
+    {
+        for(int i = 0; i < 500; i++)
+        {
+           // StartCoroutine(countDown());
+            Debug.Log("i : " + i);
+        }
+    }
+
+    IEnumerator countDown()
+    {
+        Debug.Log("COUNTdOWN");
+        yield return new WaitForSeconds(5);
+    }
+    private void OnDestroy()
+    { if (this == instance) { instance = null; } }
+}
